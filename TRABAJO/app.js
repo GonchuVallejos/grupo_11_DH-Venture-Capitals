6c96195f057608c2b7b57b432756e9f900fa86f4
D. <punko0707@gmail.com>
2024-06-03 20:35:50 -0300

    cors

--- TRABAJO/app.js
+++ TRABAJO/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const path = require('node:path');
 const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
@@ -7,9 +7,10 @@ const session = require('express-session') // requerimos para poder utilizar ses
 const cookieParser = require('cookie-parser'); // requerimos para poder usar cookies
 
 //Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // middleWares
+app.use(cors());
 app.use(express.static("public"))
 app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 app.use(express.urlencoded({ extended: false }));
