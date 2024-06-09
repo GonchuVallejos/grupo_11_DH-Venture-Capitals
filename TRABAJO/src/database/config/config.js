e3dbb4908cfd54a1046b7cff01252f41d2fc4e2e
EnzoFicoseco <enzoficoseco2704@gmail.com>
2024-06-09 20:00:18 -0300

    mejoras footer

--- TRABAJO/src/database/config/config.js
+++ TRABAJO/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "1up_bd",
     "host": "127.0.0.1",
     "dialect": "mysql"
